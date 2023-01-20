45dd6b228556d4b762a1275780eabe8258c2af92
felipendelicia <delicia4581@gmail.com>
2023-01-20 19:52:54 -0300

    pages front

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Start from "./pages/Start";
+
 function App() {
   return (
     <div className="App">
-      hola
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/" element={<Start/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
